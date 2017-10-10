ca1e375e4311c09a9c8dc23ae55c90bf142cfb0f
CoderDream <coderdream@gmail.com>
2017-10-10 11:14:35 +0800

    update

--- sqlite/html5TestJS.js
+++ sqlite/html5TestJS.js
@@ -61,7 +61,7 @@ function showAllData(){
 	xhr.onload = function(e) {
 	  var uInt8Array = new Uint8Array(this.response);
 	  var db = new SQL.Database(uInt8Array);
-	  var contents = db.exec("SELECT * FROM my_table");
+	  var contents = db.exec("SELECT * FROM COMPANY");
 	  // contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
 	};
 	xhr.send();
